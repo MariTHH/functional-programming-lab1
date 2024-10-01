f40f33b86ba690aae8954178d763807b3fca5cf5
tolst <tolstykh_mariia@mail.ru>
2024-10-01 19:17:07 +0300

    fix

--- FsSolution/Problem26.fs
+++ FsSolution/Problem26.fs
@@ -4,32 +4,31 @@ module Problem26
 let cycleLength d =
     let rec findCycle rem prevRemainders =
         match rem with
-        | 0 -> 0 
+        | 0 -> 0
         | _ when Map.containsKey rem prevRemainders -> prevRemainders.Count - prevRemainders.[rem] // Строка 6: Найден период, длина цикла
         | _ ->
             let newRem = (rem * 10) % d
             let updatedRemainders = Map.add rem prevRemainders.Count prevRemainders
-            findCycle newRem updatedRemainders 
-            
-    findCycle 1 Map.empty 
+            findCycle newRem updatedRemainders
 
-// 1. Простая рекурсия 
+    findCycle 1 Map.empty
+
+// 1. Простая рекурсия
 let findMaxCycleRecursively () =
-    Seq.init 999 (fun i -> i + 2)
-    |> Seq.maxBy cycleLength
+    Seq.init 999 (fun i -> i + 2) |> Seq.maxBy cycleLength
 
-// 2. Хвостовая рекурсия 
+// 2. Хвостовая рекурсия
 let findMaxCycleTailRecursive () =
     let rec findMax divisor maxDivisor maxLength =
         match divisor with
-        | d when d > 1000 -> maxDivisor 
+        | d when d > 1000 -> maxDivisor
         | _ ->
             let currentLength = cycleLength divisor
 
             if currentLength > maxLength then
-                findMax (divisor + 1) divisor currentLength 
+                findMax (divisor + 1) divisor currentLength
             else
-                findMax (divisor + 1) maxDivisor maxLength 
+                findMax (divisor + 1) maxDivisor maxLength
 
     findMax 2 0 0
 
@@ -39,7 +38,7 @@ let findMaxCycleModular () =
     let cycleLengths = divisors |> Seq.map cycleLength
     Seq.zip divisors cycleLengths |> Seq.maxBy snd |> fst
 
-// 4. С использованием `map` 
+// 4. С использованием `map`
 let findMaxCycleWithMap () =
     Seq.init 999 (fun i -> i + 2)
     |> Seq.map (fun d -> (d, cycleLength d))
